2e87243604139a77c831468a909d9c6877d3c511
gabriele-bellini <216871851+gabriele-bellini@users.noreply.github.com>
2026-03-17 13:23:44 +0000

    refactor: reorganize philosophers fuzzer to match paper standard structure

--- DiningPhilosophers/code/philosophers_fuzzer.asm
+++ DiningPhilosophers/code/philosophers_fuzzer.asm
@@ -6,16 +6,25 @@ import philosophers_signatureAndRules
 //declare universes and functions
 signature:
 	controlled steps: Integer
+	controlled run: Integer
 	controlled exitError: Integer
 	derived inv: Boolean
+	derived isRunTerminated: Boolean
+	static maxRun: Integer
+	static maxStep: Integer
 	
 definitions:
+	// FUNCTION DEFINITIONS
+	function maxRun = 1000
+	function maxStep = 3500
+	function isRunTerminated = (steps>=maxStep)
 	function inv = (exist $phil in PhilosophersID with hungry($phil)=true) 
 	
 	rule r_reinitializeSimulatedModel = par
 		forall $p in PhilosophersID do hungry($p) := true
 		forall $q in PhilosophersID do eating($q) := false
 		forall $f in ForkID do owner($f) := undef   
+		steps := 0
 	endpar
 	
 	rule r_simulatedMain = // choose one agent and run it
@@ -28,19 +37,26 @@ definitions:
 				r_reinitializeSimulatedModel[]
 				steps := steps + 1
 			endpar
+		else if exitError != 0 then	skip
 		else if not(inv) then
 			exitError := 1
-		else
+		else if not(isRunTerminated) then 
 			par
 				r_simulatedMain[]
 				steps := steps + 1
 			endpar
-		endif endif
+		else if run < maxRun then
+			par
+				run := run + 1
+				r_reinitializeSimulatedModel[]
+			endpar
+		endif endif endif endif endif
 
 
 default init initial_state:
 	function steps = -1      
-	function exitError = 0           
+	function exitError = 0      
+	function run = 1
 
 		
 		
